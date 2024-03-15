2aecc878910c26292c1a42eb6ba3bfed1598ad19
Casey Smith <clegg89@gmail.com>
2024-03-15 01:24:13 -0400

    Add bufferline

---
+++ config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+      vim.opt.termguicolors = true
+      require('bufferline').setup({
+        options = {
+          separator_style = 'slant',
+          diagnostics = 'nvim_lsp'
+        }
+      })
+    end
+  }
+}
