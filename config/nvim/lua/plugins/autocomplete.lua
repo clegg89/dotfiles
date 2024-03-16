14e2a6b40c8e2cafc8dc0c2976d38b7f78c95fc7
Casey Smith <clegg89@gmail.com>
2024-03-16 16:45:43 -0400

    Add nlsp-settings

--- config/nvim/lua/plugins/autocomplete.lua
+++ config/nvim/lua/plugins/autocomplete.lua
@@ -151,4 +151,19 @@ return {
       })
     end,
   },
+  {
+    'tamago324/nlsp-settings.nvim',
+    dependencies = {
+      'neovim/nvim-lspconfig',
+    },
+    confg = function()
+      require('nlspsettings').setup({
+        config_home = vim.fn.stdpath('config') .. '/nlsp-settings',
+        local_settings_dir = '.nlsp-settings',
+        local_settings_root_markers_fallback = { '.git' },
+        append_default_schemas = true,
+        loader = 'json'
+      })
+    end,
+  },
 }
